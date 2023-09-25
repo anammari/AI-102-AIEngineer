5be9e2550aca87b91b136895abf95207c4e056e6
Ahmad Ammari <ammariect@gmail.com>
2023-09-25 22:10:02 +1000

    added to fix git pull

--- 09-luis-app/GetIntent.cmd
+++ 09-luis-app/GetIntent.cmd
@@ -10,4 +10,4 @@ set input=%1
 set query=%input: =+%
 
 rem Use cURL to call the REST API
-curl -X GET "%endpoint%/luis/prediction/v3.0/apps/%app_id%/slots/production/predict?subscription-key=%key%&log=true&query=%query%"
+curl -X GET "https://lng-und-ai102.cognitiveservices.azure.com/luis/prediction/v3.0/apps/830ede9d-1cdc-4e88-a1b2-fcf714d138f7/slots/production/predict?subscription-key=ee0d590323a942ba88cd56aba8640fd5&log=true&query=%query%"
