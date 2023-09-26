846ac145b85b77425e94f8bb2b7bbd08d9b21110
Ahmad Ammari <ammariect@gmail.com>
2023-09-26 10:01:43 +1000

    run success

--- 12-qna/ask-question.sh
+++ 12-qna/ask-question.sh
@@ -1,6 +1,6 @@
 
-prediction_url="YOUR_ENDPOINT_URL"
-key="YOUR_KEY"
+prediction_url="https://ahm-amm-lng-srv-002.cognitiveservices.azure.com/language/:query-knowledgebases?projectName=LearnFAQ&api-version=2021-10-01&deploymentName=production"
+key="ecb9c669ab56433ab5357c6231485e93"
 
 curl -X POST $prediction_url -H "Ocp-Apim-Subscription-Key: $key" -H "Content-Type: application/json" -d "{'question': 'What is a learning Path?' }"
 
